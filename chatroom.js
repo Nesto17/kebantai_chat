cb8dd0d30edb0ec6414f2e2a40857ea7c1ee20dc
Jos02378 <57058570+Jos02378@users.noreply.github.com>
2021-04-13 08:48:41 +0700

    Fix issue for deleting member

--- chatroom.js
+++ chatroom.js
@@ -327,7 +327,6 @@
 
   function delete_pending_2(data) {
     if ((lastClick5 + delay2) < Date.now()) {
-      console.log("delete_pending_2 EXECUTED");
       if (data.owner == "1fj3C0p3vowY8tCrpHNa") {
         let doc_pending_data = data.pending;
         let pending_list_member = [];
@@ -380,19 +379,28 @@
         snapshot.docs.forEach(dok => {
           let current_member_database = dok.data().matches_join;
           let current_pending_database = dok.data().pending;
+          console.log("current_member_database", current_pending_database);
+          console.log("first_member", first_member);
           current_member_database.push(dok.data().owner);
 
           // FOR MEMBER (CHECK IF THERE IS MORE PEOPLE AT DATABASE)
           if (current_member_database.length < first_member.length) {
             let name_to_delete = arr_diff(current_member_database, first_member);
+            console.log("name to delete", name_to_delete);
             let element_to_delete = document.getElementById(name_to_delete);
             // FIND ELEMENT THEN DELETE IT
             if (element_to_delete) {
               document.getElementById(name_to_delete).remove();
-              let index_of_name_to_delete = first_member.indexOf(name_to_delete);
-              first_member.splice(index_of_name_to_delete, 1);
+              name_to_delete.forEach(name => {
+                let index_of_name_to_delete = first_member.indexOf(name);
+                console.log("index_of_name_to_delete", index_of_name_to_delete);
+                first_member.splice(index_of_name_to_delete, 1);
+              })
             }
           }
+
+          console.log("after1", current_member_database);
+          console.log("after2", first_member);
 
           /*******************************************************************************************/
           // DELETE PENDING
